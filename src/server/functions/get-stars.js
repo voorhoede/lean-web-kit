9b215db502ef8b90e4894adc6c0b90f83faf24a9
Jasper Moelker <j.b.moelker@activeids.nl>
2018-06-13 17:23:21 +0200

    Add demo cloud function get-stars
    to illustrate how to use env variables and do requests

--- src/server/functions/get-stars.js
+++ src/server/functions/get-stars.js
@@ -3,6 +3,18 @@ import request from 'request-promise-native'
 
 dotenv.config()
 
+const { npm_package_repository_url } = process.env
+const apiUrl = npm_package_repository_url
+  .replace('git+https://github.com/', 'https://api.github.com/repos/')
+  .replace(/\.git$/, '')
+
 export const handler = (event, context, callback) => {
-  callback(null, { statusCode: 200, body: JSON.stringify({ stars: 5 }) })
+  const send = body => callback(null, { statusCode: 200, body: JSON.stringify(body) })
+
+  request({
+      url: apiUrl,
+      headers: { 'User-Agent': 'request' },
+      json: true,
+    })
+    .then(data => send({ stars: data.stargazers_count }))
 }
