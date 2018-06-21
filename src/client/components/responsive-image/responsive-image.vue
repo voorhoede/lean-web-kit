1364d886d6e10fcf04377d4e7c18048719424165
Sanne Veroude <sanne@voorhoede.nl>
2018-06-21 11:30:42 +0200

    feat(responsive-image): add fixed ratio

--- src/client/components/responsive-image/responsive-image.vue
+++ src/client/components/responsive-image/responsive-image.vue
@@ -1,6 +1,10 @@
 <template>
   <figure>
-    <img :alt="image.alt" :src="image.url" />
+    <div class="fixed-ratio">
+      <div class="fixed-ratio__content">
+        <img :alt="image.alt" :src="image.url" />
+      </div>
+    </div>
     <figcaption v-if="image.title">{{ image.title }}</figcaption>
   </figure>
 </template>
@@ -14,5 +18,22 @@ export default {
 <style scoped>
 img {
   max-width: 100%;
+}
+
+.fixed-ratio {
+    display: block;
+    position: relative;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 56.25%;
+}
+
+.fixed-ratio__content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    max-width: inherit;
 }
 </style>
