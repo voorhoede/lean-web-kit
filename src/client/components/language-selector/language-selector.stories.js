6476d0afde3dccc0f1d74e74ca7b55435c0fc580
Jasper Moelker <j.b.moelker@activeids.nl>
2018-06-21 17:15:31 +0200

    Show component readme next to preview in guide

--- src/client/components/language-selector/language-selector.stories.js
+++ src/client/components/language-selector/language-selector.stories.js
@@ -1,8 +1,11 @@
 import VueI18n from 'vue-i18n'
 import { storiesOf } from '@storybook/vue'
+import { withReadme } from 'storybook-readme'
+import readme from './readme.md'
 import LanguageSelector from './'
 
 storiesOf('Language selector', module)
+  .addDecorator(withReadme(readme))
   .add('Default', () => ({
       components: { LanguageSelector },
       template: `<language-selector :locales="locales" />`,
