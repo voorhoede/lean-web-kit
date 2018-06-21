6a38293970bc6d044ec7f3406c14cde98bef929f
bruna <brunadafonseca@gmail.com>
2018-06-21 15:10:20 +0200

    Use BEM for list item class

--- src/client/components/table-of-contents/table-of-contents.vue
+++ src/client/components/table-of-contents/table-of-contents.vue
@@ -2,7 +2,7 @@
   <nav class="table-of-contents" id="table-of-contents">
     <h2 class="a11y-sr-only">{{ $t('table_of_contents') }}</h2>
     <ol>
-      <li v-for="(item, index) in items" :key="index">
+      <li class="table-of-contents__item" v-for="(item, index) in items" :key="index">
         <a :href="`#${item.slug}`">
           {{ item.title }}
         </a>
@@ -23,7 +23,7 @@ export default {
   margin: 0 2rem;
 }
 
-.table-of-contents li {
+.table-of-contents__item {
   padding: .7rem 0;
 }
 </style>
