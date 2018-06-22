56223b3f4d420054b0b15a7cac3da83e99c2846d
Sanne Veroude <sanne@voorhoede.nl>
2018-06-22 10:25:29 +0200

    refactor(responsive-image): replace with more descriptive class names

--- src/client/components/responsive-image/responsive-image.vue
+++ src/client/components/responsive-image/responsive-image.vue
@@ -1,17 +1,15 @@
 <template>
-  <figure>
-    <div class="fixed-ratio">
-      <div class="fixed-ratio__content">
-        <picture>
-          <!--[if IE 9]><video style="display: none;"><![endif]-->
-          <source type="image/webp" media="(min-width: 400px)" :srcset="`${image.url}?fm=webp`">
-          <source type="image/webp" :srcset="`${image.url}?w400&h=225&fm=webp`">
-          <source media="(min-width: 400px)" :srcset="image.url">
-          <source :srcset="`${image.url}?w400&h=225`">
-          <!--[if IE 9]></video><![endif]-->
-          <img :alt="image.alt" :src="image.url" />
-        </picture>
-      </div>
+  <figure class="responsive-image">
+    <div class="responsive-image__wrapper">
+      <picture class="responsive-image__content">
+        <!--[if IE 9]><video style="display: none;"><![endif]-->
+        <source type="image/webp" media="(min-width: 400px)" :srcset="`${image.url}?fm=webp`">
+        <source type="image/webp" :srcset="`${image.url}?w400&h=225&fm=webp`">
+        <source media="(min-width: 400px)" :srcset="image.url">
+        <source :srcset="`${image.url}?w400&h=225`">
+        <!--[if IE 9]></video><![endif]-->
+        <img :alt="image.alt" :src="image.url" />
+      </picture>
     </div>
     <figcaption v-if="image.title">{{ image.title }}</figcaption>
   </figure>
@@ -28,7 +26,7 @@ img {
   max-width: 100%;
 }
 
-.fixed-ratio {
+.responsive-image__wrapper {
     display: block;
     position: relative;
     height: 0;
@@ -36,7 +34,7 @@ img {
     padding-bottom: 56.25%;
 }
 
-.fixed-ratio__content {
+.responsive-image__content {
     position: absolute;
     top: 0;
     left: 0;
