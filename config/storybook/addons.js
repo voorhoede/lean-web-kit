50d067040f6edfec5aff03471ac9fd16f7e792f1
Sanne Veroude <sanne@voorhoede.nl>
2018-06-22 16:22:04 +0200

    feat(a11y-addon): add addon

--- config/storybook/addons.js
+++ config/storybook/addons.js
@@ -1,6 +1,7 @@
 // note: order of registration is order in which addons appear as tabs in panel
 import 'storybook-readme/register'
 import '@storybook/addon-knobs/register'
+import '@storybook/addon-a11y/register';
 import '@storybook/addon-actions/register'
 import '@storybook/addon-links/register'
 import '@storybook/addon-options/register'
