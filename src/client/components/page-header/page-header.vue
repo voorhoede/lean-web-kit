b7c9d500b3c3f48e334f3807f841cc23d65d7b65
bruna <brunadafonseca@gmail.com>
2018-06-25 10:59:07 +0200

    Style page header component

--- src/client/components/page-header/page-header.vue
+++ src/client/components/page-header/page-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="page-header" :style="image ? 'background-image:' + image.url + ';' : null">
+  <header class="page-header" :class="image ? 'with-cover-image' : null" :style="image ? 'background-image:url(' + image.url + ');' : null">
     <h1 class="page-header__title">{{ title }}</h1>
     <p class="page-header__subtitle" v-if="subtitle">{{ subtitle }}</p>
   </header>
@@ -19,6 +19,8 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: var(--spacing-double) var(--spacing-default);
+  position: relative;
 }
 
 .page-header:after {
@@ -26,15 +28,22 @@ export default {
   width: 40%;
   max-width: 250px;
   border: 1px solid var(--action-color);
+  position: absolute;
+  bottom: 0;
 }
 
 .page-header__title {
  font-size: 3rem;
- padding: var(--spacing-double) 0 3rem 0;
+ padding: var(--spacing-double) 0;
 }
 
 .page-header__subtitle {
+  margin-bottom: var(--spacing-double);
+  font-weight: 300;
+  text-align: center;
   font-size: var(--font-size-medium);
+  letter-spacing: .3px;
+  line-height: 1.3;
 }
 
 @media screen and (min-width: 500px) {
@@ -43,7 +52,37 @@ export default {
   }
 
   .page-header__subtitle {
+    max-width: 80%;
     font-size: var(--font-size-large);
+    letter-spacing: .8px;
+    line-height: 1.4;
   }
+}
+
+.with-cover-image {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+}
+
+.with-cover-image:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,.4);
+  width: 100%;
+  height: 100%;
+}
+
+.with-cover-image:after {
+  border: none;
+}
+
+.with-cover-image .page-header__title,
+.with-cover-image .page-header__subtitle {
+  color: var(--background-color);
+  z-index: 1;
 }
 </style>
