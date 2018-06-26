39c32df0a6bce3c8682a9240a1c6310e89ea53b3
bruna <brunadafonseca@gmail.com>
2018-06-26 09:27:01 +0200

    Add basic style to app header

--- src/client/components/app-header/app-header.vue
+++ src/client/components/app-header/app-header.vue
@@ -1,12 +1,13 @@
 <template>
   <header role="banner" class="app-header">
-    <nuxt-link :to="localePath('index') + '/'">
-      <h1>Lean Web Kit</h1>
+    <nuxt-link :to="localePath('index')" class="app-header__identity">
+      <img class="app-header__logo" src="/images/logo.svg" alt="" />
+      <h1 class="app-header__title">Lean Web Kit</h1>
     </nuxt-link>
-    <a :href="`#${contentId}`">{{ $t('skip_to_content') }}</a>
-    <nav>
+    <a class="a11y-sr-only" :href="`#${contentId}`">{{ $t('skip_to_content') }}</a>
+    <nav class="app-header__navigation">
       <h2 class="a11y-sr-only">{{ menu.title }}</h2>
-      <ul class="inline-list">
+      <ul class="app-header__inline-list">
         <li v-for="item in menu.items" :key="item.slug">
           <nuxt-link :to="localePath({ name: 'slug', params: { slug: item.slug } })"
             class="app-header__menu-link"
@@ -40,22 +41,62 @@ export default {
 <style>
 @import '../app-core/index.css';
 
-.app-header__menu-link {
+.app-header {
+  width: 100%;
+  height: var(--app-header-height);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  left: 0;
+  padding: 0 1rem;
+  background-color: #fff;
+  opacity: .9;
+  box-shadow: 0 2px 15px 0 rgba(214,214,214,.5);
+  z-index: 10000;
+}
+
+.app-header + * {
+  margin-top: var(--app-header-height);
+}
+
+.app-header__identity {
+  display: flex;
+  align-items: center;
+  color: #000;
   text-decoration: none;
 }
 
+.app-header__inline-list {
+  display: flex;
+}
+
+.app-header__logo {
+  height: 50px;
+  transform: rotate(35deg);
+}
+
+.app-header__title {
+  font-size: 1.125rem;
+}
+
 .app-header__menu-link {
-  border-bottom-width: 2px;
-  border-bottom-style: solid;
-  border-bottom-color: transparent;
+  margin: 0 1rem;
+  padding-bottom: 1rem;
+  font-size: .875rem;
+  font-weight: bold;
+  color: #000;
+  text-transform: uppercase;
+  text-decoration: none;
 }
 
 .app-header__menu-link:focus,
 .app-header__menu-link:hover {
-  color: var(--action-color);
+  border-bottom: 2px solid var(--action-color);
 }
 
-.app-header__menu-link.nuxt-link-active {
-  border-bottom-color: var(--action-color);
+.app-header__navigation {
+  display: flex;
 }
 </style>
