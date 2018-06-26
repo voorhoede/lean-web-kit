82ad27e8fa9cf6f9de3bd765197d4d33bf6e8d7d
bruna <brunadafonseca@gmail.com>
2018-06-26 15:29:06 +0200

    Add styles (mobile)

--- src/client/components/app-header/app-header.vue
+++ src/client/components/app-header/app-header.vue
@@ -5,19 +5,31 @@
       <h1 class="app-header__title">Lean Web Kit</h1>
     </nuxt-link>
     <a class="a11y-sr-only" :href="`#${contentId}`">{{ $t('skip_to_content') }}</a>
-    <nav class="app-header__navigation">
-      <h2 class="a11y-sr-only">{{ menu.title }}</h2>
-      <ul class="app-header__inline-list">
-        <li v-for="item in menu.items" :key="item.slug">
-          <nuxt-link :to="localePath({ name: 'slug', params: { slug: item.slug } })"
-            class="app-header__menu-link"
-          >
-            {{ item.title }}
-          </nuxt-link>
-        </li>
-      </ul>
-      <language-selector :locales="$i18n.locales" />
-    </nav>
+    <transition name="slide-in">
+      <nav class="app-header__navigation">
+        <h2 class="a11y-sr-only">{{ menu.title }}</h2>
+        <ul class="app-header__inline-list">
+          <li class="app-header__list-item" v-for="item in menu.items" :key="item.slug">
+            <nuxt-link :to="localePath({ name: 'slug', params: { slug: item.slug } })"
+              class="app-header__menu-link"
+            >
+              {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+        <language-selector :locales="$i18n.locales" />
+      </nav>
+    </transition>
+    <button class="app-header__toggle-menu" @click="toggleNavigation" >
+      <span class="a11y-sr-only">Toggle navigation</span>
+      <svg class="menu__icon" :class="{ 'is-open': showNavigation }" aria-hidden="true" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100">
+        <g>
+          <path class="line line-1" fill="#000000" d="M5 13h90v14H5z"/>
+          <path class="line line-2" fill="#000000" d="M5 43h90v14H5z"/>
+          <path class="line line-3" fill="#000000" d="M5 73h90v14H5z"/>
+        </g>
+      </svg>
+    </button>
   </header>
 </template>
 
@@ -29,36 +41,51 @@ export default {
   components: { LanguageSelector },
   props: ['contentId'],
   data () {
-    return { menuI18n }
+    return { 
+      menuI18n,
+      windowWidth: 0,
+      showNavigation: false,
+    }
   },
   computed: {
     locale() { return this.$i18n.locale },
     menu() { return this.menuI18n[this.locale] },
   },
+  methods: {
+    toggleNavigation () {
+      this.showNavigation = !this.showNavigation
+    }
+  }
+
 }
 </script>
 
 <style>
 @import '../app-core/index.css';
 
+.app-header__identity:hover {
+  border: none;
+}
+
+.app-header__toggle-menu {
+  width: 40px;
+  height: 40px;
+  padding: 0;
+  border: none;
+}
+
 .app-header {
-  width: 100%;
-  height: var(--app-header-height);
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: sticky;
   top: 0;
   left: 0;
-  padding: 0 1rem;
   background-color: #fff;
-  opacity: .9;
+  width: 100%;
+  height: var(--app-header-height);
   box-shadow: 0 2px 15px 0 rgba(214,214,214,.5);
-  z-index: 10000;
-}
-
-.app-header + * {
-  margin-top: var(--app-header-height);
+  z-index: 1;
 }
 
 .app-header__identity {
@@ -68,8 +95,24 @@ export default {
   text-decoration: none;
 }
 
+.app-header__navigation {
+  position: absolute;
+  top: var(--app-header-height);
+  width: 100%;
+  background-color: #fff;
+  padding: 1rem;
+  box-shadow: 0px 11px 14px rgba(214,214,214,.3);
+}
+
 .app-header__inline-list {
-  display: flex;
+  padding: 0;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+.app-header__list-item {
+  list-style-type: none;
+  padding: 1rem 0;
 }
 
 .app-header__logo {
@@ -82,11 +125,8 @@ export default {
 }
 
 .app-header__menu-link {
-  margin: 0 1rem;
   padding-bottom: 1rem;
-  font-size: .875rem;
-  font-weight: bold;
-  color: #000;
+  color: var(--text-color);
   text-transform: uppercase;
   text-decoration: none;
 }
@@ -96,7 +136,23 @@ export default {
   border-bottom: 2px solid var(--action-color);
 }
 
-.app-header__navigation {
-  display: flex;
+.is-open .line-1 {
+  transform: rotate(45deg) translateY(0) translateX(0);
+  transform-origin: 1em 2.7em;
+
+}
+
+.is-open .line-2 {
+  opacity: 0;
+}
+
+.is-open .line-3 {
+  transform: rotate(-45deg) translateY(0em) translateX(0em);
+  transform-origin: 1em 3.7em;
+}
+
+.is-open .line,
+.line {
+  transition: all .2s ease-in-out;
 }
 </style>
