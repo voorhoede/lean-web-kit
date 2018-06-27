ef2c84a3e961eee345a472d2bd8871b03157a1c1
bruna <brunadafonseca@gmail.com>
2018-06-27 15:27:12 +0200

    Create stories for different variations

--- src/client/components/app-header/app-header.stories.js
+++ src/client/components/app-header/app-header.stories.js
@@ -14,6 +14,37 @@ storiesOf('App header', module)
         }
       },
     }),
+  }))
+  .add('With action button', () => ({
+    components: { AppHeader },
+    template: '<app-header :action="action" />',
+    data () {
+      return {
+        action: {
+          title: "Sign up",
+        }
+      }
+    },
+    i18n: new VueI18n({
+      locale: 'en',
+      messages: {
+        en: {
+          skip_to_content: 'Skip to content',
+        }
+      },
+    }),
+  }))
+  .add('With language selection', () => ({
+    components: { AppHeader },
+    template: '<app-header />',
+    i18n: new VueI18n({
+      locale: 'en',
+      messages: {
+        en: {
+          skip_to_content: 'Skip to content',
+        }
+      },
+    }),
     created() {
       // mock locales required for language selector
       this.$i18n.locales = ['en', 'nl'].map(locale => ({
@@ -21,5 +52,32 @@ storiesOf('App header', module)
         iso: locale,
         name: locale,
       }))
+  }
+}))
+.add('With action button and language selection', () => ({
+  components: { AppHeader },
+  template: '<app-header :action="action" />',
+  data () {
+    return {
+      action: {
+        title: "Sign up",
+      }
     }
-  }))
+  },
+  i18n: new VueI18n({
+    locale: 'en',
+    messages: {
+      en: {
+        skip_to_content: 'Skip to content',
+      }
+    },
+  }),
+  created() {
+    // mock locales required for language selector
+    this.$i18n.locales = ['en', 'nl'].map(locale => ({
+      code: locale,
+      iso: locale,
+      name: locale,
+    }))
+  }
+}))
