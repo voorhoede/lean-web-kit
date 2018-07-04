d07933c1f5e80a8f023309d1f25f916974634a28
bruna <brunadafonseca@gmail.com>
2018-07-04 14:13:05 +0200

    Add passive listener

--- src/client/components/table-of-contents/table-of-contents.vue
+++ src/client/components/table-of-contents/table-of-contents.vue
@@ -21,7 +21,7 @@ export default {
     }
   },
   mounted () {
-    window.addEventListener('scroll', this.updateActiveItem)
+    window.addEventListener('scroll', this.updateActiveItem, { passive: true })
   },
   
   beforeDestroy () {
