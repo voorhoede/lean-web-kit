3c9123921de4f9f86d90fde22ece0f4afa7f764d
bruna <brunadafonseca@gmail.com>
2018-07-04 13:45:53 +0200

    Simplify naming

--- src/client/components/table-of-contents/table-of-contents.vue
+++ src/client/components/table-of-contents/table-of-contents.vue
@@ -2,9 +2,9 @@
   <nav class="table-of-contents">
     <h2 class="a11y-sr-only">{{ $t('table_of_contents') }}</h2>
     
-    <ol class="table-of-contents__inline-list">
-      <li class="table-of-contents__list-item" v-for="(item, index) in items" :key="index">
-        <a :href="`#${item.slug}`" class="table-of-contents__link-item" :class="{ 'active' : (item.slug === currentSection) }">
+    <ol class="table-of-contents__list">
+      <li class="table-of-contents__item" v-for="(item, index) in items" :key="index">
+        <a :href="`#${item.slug}`" class="table-of-contents__link" :class="{ 'active' : (item.slug === currentSection) }">
           {{ item.title }}
         </a>
       </li>
@@ -55,21 +55,21 @@ export default {
   display: block;
 }
 
-.table-of-contents__inline-list {
+.table-of-contents__list {
   padding: 0;
   text-align: left;
 }
 
-.table-of-contents__list-item {
+.table-of-contents__item {
   list-style-type: none;
   text-transform: uppercase;
 }
 
-.table-of-contents__list-item:not(:first-child) .table-of-contents__link-item {
+.table-of-contents__item:not(:first-child) .table-of-contents__link {
   padding-top: var(--spacing-default);
 }
 
-.table-of-contents__link-item {
+.table-of-contents__link {
   display: block;
   padding-bottom: var(--spacing-default);
   color: var(--text-color);
@@ -81,8 +81,8 @@ export default {
           transition: 1s;
 }
 
-.table-of-contents__link-item:hover,
-.table-of-contents__link-item:focus,
+.table-of-contents__link:hover,
+.table-of-contents__link:focus,
 .active {
   border: none;
   outline: none;
