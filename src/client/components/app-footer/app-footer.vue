8c3dc0d4b95c618a4a1cfdc308c1f7ed0d3e277b
Sanne Veroude <sanne@voorhoede.nl>
2018-07-05 12:55:02 +0200

    feat(app-footer): add copywright info

--- src/client/components/app-footer/app-footer.vue
+++ src/client/components/app-footer/app-footer.vue
@@ -1,63 +1,69 @@
 <template>
   <footer role="contentinfo" class="app-footer">
-    <div>
-      <nuxt-link :to="localePath('index') + '/'" class="app-footer__identity">
-        <img class="app-footer__logo" src="/images/logo.svg" alt="" />
-        <h1 class="app-footer__title">Lean Web Kit</h1>
-      </nuxt-link>
-      <div class="app-footer__social">
-        <a class="app-footer__social-link" v-if="social.facebook" :href="social.facebook" @click="track(social.facebook)">
-          <facebook-icon />
-          <span class="a11y-sr-only">Facebook</span>
-        </a>
-        <a class="app-footer__social-link" v-if="social.twitter" :href="social.twitter" @click="track(social.twitter)">
-          <twitter-icon />
-          <span class="a11y-sr-only">Twitter</span>
-        </a>
-        <a class="app-footer__social-link" v-if="social.googlePlus" :href="social.googlePlus" @click="track(social.googlePlus)">
-          <google-plus-icon />
-          <span class="a11y-sr-only">Google Plus</span>
-        </a>
-        <a class="app-footer__social-link" v-if="social.instagram" :href="social.instagram" @click="track(social.instagram)">
-          <instagram-icon />
-          <span class="a11y-sr-only">Instagram</span>
-        </a>
-        <a class="app-footer__social-link" v-if="social.youtube" :href="social.youtube" @click="track(social.youtube)">
-          <youtube-icon />
-          <span class="a11y-sr-only">YouTube</span>
-        </a>
-        <a class="app-footer__social-link" v-if="social.linkedin" :href="social.linkedin" @click="track(social.linkedin)">
-          <linkedin-icon />
-          <span class="a11y-sr-only">LinkedIn</span>
-        </a>
+    <div class="app-footer__container">
+      <div>
+        <nuxt-link :to="localePath('index') + '/'" class="app-footer__identity">
+          <img class="app-footer__logo" src="/images/logo.svg" alt="" />
+          <h1 class="app-footer__title">Lean Web Kit</h1>
+        </nuxt-link>
+        <div class="app-footer__social">
+          <a class="app-footer__social-link" v-if="social.facebook" :href="social.facebook" @click="track(social.facebook)">
+            <facebook-icon />
+            <span class="a11y-sr-only">Facebook</span>
+          </a>
+          <a class="app-footer__social-link" v-if="social.twitter" :href="social.twitter" @click="track(social.twitter)">
+            <twitter-icon />
+            <span class="a11y-sr-only">Twitter</span>
+          </a>
+          <a class="app-footer__social-link" v-if="social.googlePlus" :href="social.googlePlus" @click="track(social.googlePlus)">
+            <google-plus-icon />
+            <span class="a11y-sr-only">Google Plus</span>
+          </a>
+          <a class="app-footer__social-link" v-if="social.instagram" :href="social.instagram" @click="track(social.instagram)">
+            <instagram-icon />
+            <span class="a11y-sr-only">Instagram</span>
+          </a>
+          <a class="app-footer__social-link" v-if="social.youtube" :href="social.youtube" @click="track(social.youtube)">
+            <youtube-icon />
+            <span class="a11y-sr-only">YouTube</span>
+          </a>
+          <a class="app-footer__social-link" v-if="social.linkedin" :href="social.linkedin" @click="track(social.linkedin)">
+            <linkedin-icon />
+            <span class="a11y-sr-only">LinkedIn</span>
+          </a>
+        </div>
+      </div>
+      <div class="app-footer__content">
+        <nav class="app-footer__nav">
+          <h2 class="a11y-sr-only">{{ menu.title }}</h2>
+          <ul class="flat-list">
+            <li class="app-footer__nav-item" v-for="item in menu.items" :key="item.slug">
+              <nuxt-link :to="localePath({ name: 'slug', params: { slug: item.slug } })"
+                class="app-footer__nav-link"
+              >
+                {{ item.title }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </nav>
+        <section class="app-footer__contact">
+          <h2 class="app-footer__section-title">Contact</h2>
+          <span class="app-footer__contact-line"><a :href="`mailto:${contact.emailAddress}`" class="app-footer__contact-link">{{ contact.emailAddress }}</a></span>
+          <span class="app-footer__contact-line app-footer__contact-line--emphasis"><a :href="`tel:${contact.phoneNumber}`" class="app-footer__contact-link">{{ contact.phoneNumber }}</a></span>
+          <span class="app-footer__contact-line">{{ contact.street }}</span>
+          <span class="app-footer__contact-line">{{ contact.city }}</span>
+        </section>
       </div>
     </div>
-    <div class="app-footer__content">
-      <nav class="app-footer__nav">
-        <h2 class="a11y-sr-only">{{ menu.title }}</h2>
-        <ul class="flat-list">
-          <li class="app-footer__nav-item" v-for="item in menu.items" :key="item.slug">
-            <nuxt-link :to="localePath({ name: 'slug', params: { slug: item.slug } })"
-              class="app-footer__nav-link"
-            >
-              {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </nav>
-      <section class="app-footer__contact">
-        <h2 class="a11y-sr-only">Contact</h2>
-        <span class="app-footer__contact-line"><a :href="`mailto:${contact.emailAddress}`" class="app-footer__contact-link">{{ contact.emailAddress }}</a></span>
-        <span class="app-footer__contact-line"><a :href="`tel:${contact.phoneNumber}`" class="app-footer__contact-link">{{ contact.phoneNumber }}</a></span>
-        <span class="app-footer__contact-line">{{ contact.street }}</span>
-        <span class="app-footer__contact-line">{{ contact.city }}</span>
-      </section>
+    <div class="app-footer__copywright">
+      {{ appConfig.title }} {{ new Date().getFullYear() }} - All rights reserved
     </div>
   </footer>
 </template>
 
 <script>
 
+import appConfig from '../../static/data/app.json'
 import contact from '../../static/data/contact.json'
 import social from '../../static/data/social.json'
 import menuI18n from '../../static/data/menu.json'
@@ -74,7 +80,7 @@ export default {
   props: ['contentId'],
   components: { FacebookIcon, TwitterIcon, GooglePlusIcon, InstagramIcon, LinkedinIcon, YoutubeIcon },
   data () {
-    return { menuI18n, contact, social }
+    return { appConfig, menuI18n, contact, social }
   },
   computed: {
     locale() { return this.$i18n.locale },
@@ -96,8 +102,12 @@ export default {
 <style>
 
 .app-footer {
-  padding: var(--spacing-double) var(--spacing-default);
+  padding: var(--spacing-double) var(--spacing-default) var(--spacing-default);
   background-color: var(--neutral-color);
+}
+
+.app-footer__container {
+  margin-bottom: var(--spacing-double);
 }
 
 .app-footer__identity {
@@ -125,6 +135,12 @@ export default {
   text-align: center;
 }
 
+.app-footer__section-title {
+  margin-bottom: var(--spacing-default);
+  text-transform: uppercase;
+  font-size: var(--font-size-medium);
+}
+
 .app-footer__social {
   margin-bottom: var(--spacing-double);
   text-align: center;
@@ -149,22 +165,31 @@ export default {
   margin-bottom: var(--spacing-double);
 }
 
-.app-footer__nav-link {
-  color: var(--text-color);
-  font-size: var(--font-size-small);
-  line-height: 2rem;
-}
-
 @media (min-width: 480px) {
   .app-footer__nav {
     margin-bottom: 0;
   }
 }
 
+.app-footer__nav-link {
+  color: var(--text-color);
+  font-size: var(--font-size-small);
+  line-height: 2rem;
+}
+
+.app-footer__copywright {
+  font-size: var(--font-size-small);
+  text-align: center;
+}
+
 .app-footer__contact-line {
   display: block;
   font-size: var(--font-size-small);
   line-height: 1.5rem;
+}
+
+.app-footer__contact-line--emphasis {
+  font-weight: bold;
 }
 
 .app-footer__contact-link {
@@ -192,13 +217,13 @@ export default {
 }
 
 @media (min-width: 960px) {
-  .app-footer {
+  .app-footer__container {
     display: flex;
     justify-content: space-evenly;
   }
 
-  .app-footer > * {
-    width: 50%;
+  .app-footer__container > div {
+    flex: 1 0 50%;
   }
   
   .app-footer__social {
@@ -206,6 +231,7 @@ export default {
   }
 
   .app-footer__content {
+    align-items: center;
     justify-content: space-between;
     padding: var(--spacing-default) 3rem;
     border-left: 2px solid var(--text-color);
