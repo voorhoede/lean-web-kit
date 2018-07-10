cb68e160deb894195ed20890497d33c636465bc3
bruna <brunadafonseca@gmail.com>
2018-07-10 16:14:16 +0200

    Remove component from index page

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -2,19 +2,12 @@
   <main>
     <h1>Lean Web Kit</h1>
     <p>...</p>
-    <share-button
-      :title="'Test title'"
-      :url="'www.voorhoede.nl'"
-      :description="'This is the website description'"
-    />
   </main>
 </template>
 
 <script>
-import { ShareButton } from '../components/'
 
 export default {
-  components: { ShareButton }
 }
 </script>
 
