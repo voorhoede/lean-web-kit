24bc0ae9fc6611506f62a2b4c006bf2bd222b1d0
bruna <brunadafonseca@gmail.com>
2018-07-12 10:36:04 +0200

    Adjust z-index

--- src/client/components/app-header/app-header.vue
+++ src/client/components/app-header/app-header.vue
@@ -68,7 +68,7 @@ export default {
   position: sticky;
   top: 0;
   left: 0;
-  z-index: 1;
+  z-index: 10;
   padding: 0 var(--spacing-default);
   width: 100%;
   height: var(--app-header-mobile-height);
