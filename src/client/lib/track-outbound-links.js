e8ac44f28fca0059dd93654e9e3128ea86ad8bd4
bruna <brunadafonseca@gmail.com>
2018-07-12 15:35:07 +0200

    Remove irrelevant file

--- src/client/lib/track-outbound-links.js
+++
@@ -1,9 +0,0 @@
-export default function (destination) {
-  // https://developers.google.com/analytics/devguides/collection/analyticsjs/events#outbound_link_and_form_tracking
-  this.$ga.event({
-    eventCategory: 'Outbound Link',
-    eventAction: 'click',
-    eventLabel: destination,
-    transport: 'beacon',
-  })
-}
