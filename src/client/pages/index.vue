46ed18da0ccfc587282fd39d0866eac5d2b83f93
bruna <brunadafonseca@gmail.com>
2018-07-13 15:28:52 +0200

    Remove image

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <main>
-    <page-header :class="'full-page-header'" :image="pageInfo.image" :title="pageInfo.title" :subtitle="pageInfo.subtitle" />
-    <!-- <h1>Lean Web Kit</h1> -->
-    <!-- <p>...</p> -->
+    <h1>Lean Web Kit</h1>
+    <p>...</p>
   </main>
 </template>
 
@@ -11,15 +10,6 @@ import { PageHeader } from '../components'
 
 export default {
   components: { PageHeader },
-  data () {
-    return {
-      pageInfo: {
-        image: { src: 'https://www.datocms-assets.com/6068/1531404759-spacex-530585-unsplash.jpg' },
-        title: 'Lean Web Kit',
-        subtitle: 'We kickstart your projects!'
-      }
-    }
-  }
 }
 </script>
 
