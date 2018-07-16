bce710be9280d0e9c236bbe153a9299c8aef7461
bruna <brunadafonseca@gmail.com>
2018-07-16 09:45:13 +0200

    Create page-layout component so its reusable for slug and index

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,13 +1,50 @@
 <template>
   <main>
-    <h1>Lean Web Kit</h1>
-    <p>...</p>
   </main>
 </template>
 
 <script>
+import { ContentSection, TableOfContents, PageHeader, SocialShare } from '../components/'
+import { getPageData, seoHead } from '../lib/'
 
 export default {
+  components: { ContentSection, TableOfContents, PageHeader, SocialShare },
+  
+  async asyncData ({ app, params, store }) {
+    const { slug } = params
+    const page = await getPageData({ slug, locale: app.i18n.locale })
+    store.commit('setSlugI18n', page.slugI18n)
+    return { page }
+  },
+
+  head () {
+    return seoHead(this.page.seo)   
+  },
+
+  data () {
+    return {
+      url: '',
+    }
+  },
+
+  computed: {
+    shareText() { return this.page.seo.description },
+    shareTitle() { return this.page.seo.title },
+  },
+
+  mounted () {
+    this.url = window.location.href
+  },
+
+  methods: {
+    shared (platform) {
+      return this.$ga.social({
+        socialNetwork: platform,
+        socialAction: 'share',
+        socialTarget: this.url
+      })
+    }
+  }
 }
 </script>
 
