8ea0a442c7b4cb129df7267b2190958de3b5e776
Jurgen Beliën <jurgen@voorhoede.nl>
2018-07-23 15:06:44 +0200

    refactor(modules): add name and condition inside modules

--- config/nuxt/modules/google-analytics.js
+++ config/nuxt/modules/google-analytics.js
@@ -4,22 +4,25 @@ const isProduction = (process.env.NODE_ENV === 'production')
 /**
  * https://github.com/nuxt-community/analytics-module
  */
-module.exports = {
-  id: appConfig.googleAnalyticsTrackingId,
-  /**
-   * Debug while in development mode
-   * @see https://matteogabriele.gitbooks.io/vue-analytics/content/docs/debug.html
-   */
-  debug: {
-    enabled: !isProduction,
-    sendHitTask: isProduction,
-  },
-  /**
-   * Anonymize tracking
-   * @see https://www.themarketingtechnologist.co/setting-up-a-cookie-law-compliant-google-analytics-tracker/
-   */
-  set: [
-    { field: 'displayFeaturesTask', value: null },
-    { field: 'anonymizeIp', value: true },
-  ],
-}
+module.exports = appConfig.googleAnalyticsTrackingId &&
+  ['@nuxtjs/google-analytics',
+  {
+    id: appConfig.googleAnalyticsTrackingId,
+    /**
+     * Debug while in development mode
+     * @see https://matteogabriele.gitbooks.io/vue-analytics/content/docs/debug.html
+     */
+    debug: {
+      enabled: !isProduction,
+      sendHitTask: isProduction,
+    },
+    /**
+     * Anonymize tracking
+     * @see https://www.themarketingtechnologist.co/setting-up-a-cookie-law-compliant-google-analytics-tracker/
+     */
+    set: [
+      { field: 'displayFeaturesTask', value: null },
+      { field: 'anonymizeIp', value: true },
+    ],
+  }
+]
