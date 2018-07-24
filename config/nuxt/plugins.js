5d9e7cd6dc9994e7a78b9b642a088dc3b7bbfed3
Jurgen Beliën <jurgen@voorhoede.nl>
2018-07-24 17:18:20 +0200

    style(plugins): remove .js

--- config/nuxt/plugins.js
+++ config/nuxt/plugins.js
@@ -2,5 +2,5 @@
  * @see https://nuxtjs.org/api/configuration-plugins
  */
 module.exports = [
-  { src: '~/plugins/install-prompt.js', ssr: false },
+  { src: '~/plugins/install-prompt', ssr: false },
 ]
