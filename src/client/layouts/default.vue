4d9adb05f53860e2bf5b4b4487b04a2d6290c66f
Jurgen Beliën <jurgen@voorhoede.nl>
2018-07-27 09:29:41 +0200

    feat(default): track outbound links

--- src/client/layouts/default.vue
+++ src/client/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div @click="trackOutboundLink">
     <app-header :contentId="contentId" :menuI18n="menuI18n" />
     <main :id="contentId">
       <nuxt/>
@@ -21,6 +21,19 @@ export default {
       menuI18n,
     }
   },
+  methods: {
+    trackOutboundLink (event) {
+      const target = event.target
+      if (
+        target.hasAttribute('data-outbound')
+        && ('ga' in window)
+        && (typeof window.ga === 'function')
+      ) {
+        console.log(target.href)
+        window.ga('send', 'event', 'outbound', 'click', target.href, { 'transport': 'beacon' })
+      }
+    }
+  }
 }
 </script>
 
