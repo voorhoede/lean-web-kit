d140cc7a409e33251e45075a9ed0037d6439cbae
Anand Graves <anand@voorhoede.nl>
2018-08-09 11:04:17 +0200

    WIP

---
+++ src/client/components/contact-form/contact-form.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+    Contact form
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    form: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
