35a5c11f03f1c645d075c430536a4f7af1c0af0a
Anand Graves <anand@voorhoede.nl>
2018-08-14 17:06:23 +0200

    refactor: use process env in client, track branch in google analytics

--- config/nuxt/env.js
+++ config/nuxt/env.js
@@ -1,9 +1,15 @@
+require('dotenv-safe').config() // load env variables from .env file into process.env
+
 /**
  * Use Netlify's URL variable:
  * @see https://www.netlify.com/docs/continuous-deployment/#build-environment-variables
+ *
+ * We take variables from the Node process.env context
+ * and make them available on Nuxt's process.env context.
+ * For example `process.env.URL` from the Node context
+ * becomes `process.env.baseUrl` in the Nuxt context.
  */
-const baseUrl = process.env.URL
-
 module.exports = {
-  baseUrl
+  baseUrl: process.env.URL,
+  branch: process.env.BRANCH,
 }
