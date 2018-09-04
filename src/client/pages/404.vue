5b4eadb92cd9e0c3553713effd3a8184c146baf9
Anand Graves <anand@voorhoede.nl>
2018-09-04 11:05:03 +0200

    feat: added error and 404 page

---
+++ src/client/pages/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <error :error="error" />
+  </div>
+</template>
+
+<script>
+import Error from '../layouts/error'
+
+export default {
+  components: { Error },
+  data () {
+    return {
+      error: {
+        statusCode: 404,
+      }
+    }
+  }
+}
+</script>
