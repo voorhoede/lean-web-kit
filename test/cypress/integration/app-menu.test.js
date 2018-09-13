1c620a3d1494797744b3c67485e5abc5b32f07d6
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-09-13 11:16:05 +0200

    Add e2e testing for app menu

--- test/cypress/integration/app-menu.test.js
+++ test/cypress/integration/app-menu.test.js
@@ -1,11 +1,31 @@
-const identityLink = '[data-test="appHeaderIdentity"]'
+const appHeaderMenu = '[data-test="appHeaderMenu"]'
+const appHeaderMenuButton = '[data-test="appHeaderMenuButton"]'
+const appHeaderActionButton = '[data-test="appHeaderActionButton"]'
 
 describe('App menu', () => {
   beforeEach(() => {
     cy.visit('/')
+    cy.viewport('ipad-mini')
   })
 
-  it('should always show the logo', () => {
-    cy.get(identityLink).should('be.visible')
+  it('When I hit the menu button, the navigation should toggle on/off.', () => {
+    cy.get(appHeaderMenuButton).click()
+    cy.get(appHeaderMenu).should('be.visible')
+    cy.get(appHeaderMenuButton).click()
+    cy.get(appHeaderMenu).should('be.hidden')
+  })
+
+  it('When I press enter on the menu button, the navigation should toggle on/off.', () => {
+    cy.get(appHeaderMenuButton).focus()
+    cy.get(appHeaderMenuButton).trigger('click')
+    cy.get(appHeaderMenu).should('be.visible')
+    cy.get(appHeaderMenuButton).focus()
+    cy.get(appHeaderMenuButton).trigger('click')
+    cy.get(appHeaderMenu).should('be.hidden')
+  })
+
+  it('The mobile navigation should not contain a primary action button.', () => {
+    cy.get(appHeaderMenuButton).click()
+    cy.get(appHeaderActionButton).should('not.exist')
   })
 })
