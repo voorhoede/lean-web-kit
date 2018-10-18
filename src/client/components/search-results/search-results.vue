6752296dda356d60208ead5ae029d5cadfba1157
Anand Graves <anand@voorhoede.nl>
2018-10-18 11:10:12 +0200

    feat: added <search-results />

---
+++ src/client/components/search-results/search-results.vue
@@ -0,0 +1,36 @@
+<template>
+  <ol v-if="!closeSearchResults" class="search-results">
+    <li
+      v-for="(result, index) in results"
+      :key="index">
+      <h2 class="search-results__title">
+        <nuxt-link :to="`/${$i18n.locale}/${result.slug}`" @click.native="close">{{ result.title }}</nuxt-link>
+      </h2>
+      <rich-text :text="result.body" />
+    </li>
+  </ol>
+</template>
+
+<script>
+import RichText from '../rich-text'
+
+export default {
+  components: { RichText },
+  props: {
+    results: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      closeSearchResults: false
+    }
+  },
+  methods: {
+    close () {
+      this.closeSearchResults = true
+    }
+  }
+}
+</script>
