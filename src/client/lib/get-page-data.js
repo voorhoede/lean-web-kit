0eabeff532916658409d8ddce785c98a1404576e
Remco <remco@voorhoede.nl>
2019-01-07 13:26:27 +0100

    fix(get-page-data): use readFile instead of readFileSync to load page data

--- src/client/lib/get-page-data.js
+++ src/client/lib/get-page-data.js
@@ -1,12 +1,16 @@
+
 export default function ({ locale, slug }) {
   const url = `/data/${locale}/pages/${slug}.json`
-  
+
   if (process.client) {
     // On client load over http
     return fetch(url).then(res => res.json())
   } else {
     // On server load from file system
-    const data = JSON.parse(require('fs').readFileSync(`src/client/static${url}`, 'utf8'))
-    return Promise.resolve(data)
+    return new Promise((resolve, reject) => {
+      require('fs').readFile(`src/client/static${url}`, 'utf8', (err, data) => {
+        err ? reject(err) : resolve( JSON.parse(data) );
+      });
+    });
   }
 }
