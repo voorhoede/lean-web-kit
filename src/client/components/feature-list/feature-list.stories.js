823789acb955ad77a6fc42c5d9b35c9673262022
Anand Graves <anand@voorhoede.nl>
2019-02-11 13:41:44 +0100

    refactor(feature-list): consistent use of keys without quotes

--- src/client/components/feature-list/feature-list.stories.js
+++ src/client/components/feature-list/feature-list.stories.js
@@ -14,7 +14,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -25,7 +25,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -34,7 +34,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39141,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -43,7 +43,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39142,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -54,7 +54,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -63,7 +63,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39141,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -72,7 +72,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39142,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -81,7 +81,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39143,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -90,7 +90,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39144,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -99,7 +99,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39145,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -108,7 +108,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39146,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -117,7 +117,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39147,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -126,7 +126,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39148,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -159,7 +159,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -171,7 +171,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -181,7 +181,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39141,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -191,7 +191,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39142,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -203,7 +203,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -218,7 +218,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39141,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -233,7 +233,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39142,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -250,7 +250,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39140,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -260,7 +260,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39141,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -270,7 +270,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39142,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -280,7 +280,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39143,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -290,7 +290,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39144,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -300,7 +300,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39145,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -310,7 +310,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Plug-and-Play pages',
             id: 39146,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -320,7 +320,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Optimised for search (SEO)',
             id: 39147,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
@@ -330,7 +330,7 @@ storiesOf('Feature list', module)
             itemType: 'feature',
             title: 'Know your users',
             id: 39148,
-            "image": {
+            image: {
               "format": "svg",
               "url": "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/></svg>"
             },
