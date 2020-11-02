11d52e1fbf11ecc66185096bb47c24bd757968ae
mkMolendijk <mark.molendijk@hotmail.com>
2020-11-02 15:43:28 +0100

    refactor: added 404 page query

--- src/client/layouts/error.vue
+++ src/client/layouts/error.vue
@@ -1,23 +1,36 @@
 <template>
-  <page-layout v-if="error.statusCode === 404" :page="page" />
+  <page-layout v-if="error.statusCode === 404" :page="errorPage" />
 </template>
 
 <script>
-  import pageI18n from '../static/data/404.json'
   import { PageLayout } from '../components/'
+  import getDatoData from "../lib/get-page-data";
+  import query from "./_404.query.graphql";
 
   export default {
     components: { PageLayout },
     props: ['error'],
     layout: 'default',
-    computed: {
-      locale() { return this.$i18n.locale },
-      page() { return pageI18n[this.locale] },
+
+    data() {
+      return {
+        errorPage: {}
+      }
     },
+
+    async fetch() {
+      const locale = this.$i18n.locale
+      const { _404Page } = await getDatoData({
+        query,
+        locale
+      })
+      this.errorPage = _404Page
+    },
+
     head() {
       return {
-        title: this.page.title
+        title: this.errorPage.title
       }
-    }
+    },
   }
 </script>
